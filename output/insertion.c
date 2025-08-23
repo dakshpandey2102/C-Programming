a06f0ff5e29353130d510a353f4d0fe6d12987db
Daksh Pandey <dakshpandey748@gmail.com>
2025-08-24 00:46:59 +0530

    insertion sort

---
+++ output/insertion.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, i, pos, value;
+
+    // Initial array size
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    // Input elements
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+
+    printf("Enter position to insert (0 to %d): ", n);
+    scanf("%d", &pos);
+    printf("Enter value to insert: ");
+    scanf("%d", &value);
+
+    // Shift elements to the right
+    for(i = n; i > pos; i--) {
+        arr[i] = arr[i - 1];
+    }
+
+    // Insert the value
+    arr[pos] = value;
+    n++; 
+
+    
+    printf("Array after insertion:\n");
+    for(i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
